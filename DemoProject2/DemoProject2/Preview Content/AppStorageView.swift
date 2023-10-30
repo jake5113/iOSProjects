60e339d8cb87b8c8f6336e44e7e526eed42c1e62
jake5113 <jongwon5113@gmail.com>
2023-10-30 22:30:12 +0900

    .

---
+++ DemoProject2/DemoProject2/Preview Content/AppStorageView.swift
@@ -0,0 +1,18 @@
+//
+//  AppStorageView.swift
+//  DemoProject2
+//
+//  Created by 이종원 on 10/30/23.
+//
+
+import SwiftUI
+
+struct AppStorageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStorageView()
+}
---
+++ DemoProject2/DemoProject2/SceneAppStorageNavView.swift
@@ -0,0 +1,18 @@
+//
+//  SceneAppStorageNavView.swift
+//  DemoProject2
+//
+//  Created by 이종원 on 10/30/23.
+//
+
+import SwiftUI
+
+struct SceneAppStorageNavView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SceneAppStorageNavView()
+}

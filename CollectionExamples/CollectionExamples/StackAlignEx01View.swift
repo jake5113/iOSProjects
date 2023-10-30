6b64dbfa913932c7d947cad45983264dabad2715
jake5113 <jongwon5113@gmail.com>
2023-10-30 18:00:55 +0900

    alignment

---
+++ CollectionExamples/CollectionExamples/StackAlignEx01View.swift
@@ -0,0 +1,18 @@
+//
+//  StackAlignEx01View.swift
+//  CollectionExamples
+//
+//  Created by 이종원 on 10/30/23.
+//
+
+import SwiftUI
+
+struct StackAlignEx01View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StackAlignEx01View()
+}
---
+++ CollectionExamples/CollectionExamples/UIEx05View.swift
@@ -0,0 +1,18 @@
+//
+//  UIEx05View.swift
+//  CollectionExamples
+//
+//  Created by 이종원 on 10/30/23.
+//
+
+import SwiftUI
+
+struct UIEx05View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIEx05View()
+}
